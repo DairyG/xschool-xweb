7572e136d7a616d9b81c3ac98c67e0d777c3f5ab
张衡 <917517895@qq.com>
2019-07-20 17:24:31 +0800

    测试

--- src/js/service.js
+++ src/js/service.js
@@ -40,8 +40,9 @@ function getUserPopModel() {
 }
 
 var Serv = {
-    ServiceUrl: "http://114.116.54.157:8000/api/v1/",
-    // ServiceUrl: "http://localhost:8000/api/v1/",
+
+    //ServiceUrl: "http://114.116.54.157:8000/api/v1/",
+    ServiceUrl: "http://localhost:8000/api/v1/",
     UCenterUrl: "",
     // Code: "JDWL",//站点名称
     Token: "", //用户的Token
